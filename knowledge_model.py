862848948ba523cc1c0ae3357f0e3adca1fbec45
myname21-meet <myname21@meet.mit.edu>
2019-07-29 03:29:31 -0400

    lab 1

--- knowledge_model.py
+++ knowledge_model.py
@@ -13,5 +13,17 @@ class Knowledge(Base):
 	# The third column will be a string representing the 
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
+	__tablename__= "Articles"
+	article_url= Column(Integer, primary_key =True)
+	article_name= Column(String)
+	article_topic= Column(String)
+	article_rating= Column(Integer)
+	def __repr__(self):
+		return ("Article Url: {}\n"
+			"Article Name: {} \n"
+			"Article Topic: {} \n"
+			"Article Rating: {}").format(
+			self.article_url, self.article_name, self.article_topic, self.article_rating)
 
-	pass
+Dairy = Knowledge(article_url = "https://en.wikipedia.org/wiki/Cheese", article_name = "Cheese", article_topic = "Cheese", article_rating = 10 )
+print(Dairy)
